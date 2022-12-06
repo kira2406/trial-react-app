555787f4c7dda109311012d514469661f21e5566
Kushwanth P <Kushwanth.P@Cognizant.com>
2022-12-07 00:31:16 +0530

    "initial commit"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { Alert, Spinner } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 function App() {
+
+  const [users, setUsers] = useState([])
+
+  const fetchQuotes = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users")
+      .then(response => setUsers(response.data))
+      .catch(error => console.log(error))
+  }
+
+  useEffect(() => {
+    fetchQuotes()
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <h2>Quotes</h2>
+
+      <div>
+        {users.length > 0 ?
+          users.map(item =>
+            <Alert variant='primary'>{item?.name} - {item.email}</Alert>
+          )
+          :
+          <Spinner animation="border" />
+        }
+      </div>
+
     </div>
   );
 }
